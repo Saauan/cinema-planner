548b4bd89eec77e7dbd08cab019546c88e3b06c9
COIGNION Tristan <tristan.coignion@gmail.com>
2022-12-29 15:45:42 +0100

    refactor: increase readability of algorithm

--- planner.py
+++ planner.py
@@ -1,6 +1,6 @@
 """The planner module should be able to take a list of screenings and return a list of combinations of screenings of
 different movies that don't intersect in time."""
-from typing import List
+from typing import List, Tuple, Set
 
 MAX_MOVIES = 3
 MIN_WAIT_TIME_BETWEEN_SCREENINGS = 60
@@ -39,19 +39,13 @@ class CinemaScreening:
     def __hash__(self):
         return hash((self.movie, self.start_time_in_minutes, self.duration))
 
-def get_screenings_combinations(screenings: List[CinemaScreening], max_depth = MAX_MOVIES, time_between_screenings = MIN_WAIT_TIME_BETWEEN_SCREENINGS) -> List[List[CinemaScreening]]:
+
+def get_screenings_combinations(screenings: List[CinemaScreening], max_depth=MAX_MOVIES,
+                                time_between_screenings=MIN_WAIT_TIME_BETWEEN_SCREENINGS) -> List[Tuple[CinemaScreening]]:
     """
     Return a list of all the possible combinations of screenings of different movies that don't intersect.
 
     Each result must have more than one screening.
-
-    Examples :
-    >>> get_screenings_combinations([CinemaScreening("movie1", 0, 60), CinemaScreening("movie2", 60, 60)])
-    [[movie1 (00:00 - 01:00)], [movie2 (01:00 - 02:00)]]
-    >>> get_screenings_combinations([CinemaScreening("movie1", 0, 60), CinemaScreening("movie2", 60, 60), CinemaScreening("movie3", 120, 60)])
-    [[movie1 (00:00 - 01:00)], [movie2 (01:00 - 02:00)], [movie3 (02:00 - 03:00)], [movie1 (00:00 - 01:00), movie2 (01:00 - 02:00)], [movie1 (00:00 - 01:00), movie3 (02:00 - 03:00)], [movie2 (01:00 - 02:00), movie3 (02:00 - 03:00)]]
-    >>> get_screenings_combinations([CinemaScreening("movie1", 0, 60), CinemaScreening("movie2", 60, 60)], CinemaScreening("movie2", 30, 60)], CinemaScreening("movie2", 120, 60)])
-    [[movie1 (00:00 - 01:00)], [movie2 (01:00 - 02:00)], [movie1 (00:00 - 01:00), movie2 (02:00 - 03:00)]]
     """
     # Sorts the list of screening by start time
     screenings.sort(key=lambda screening: screening.start_time_in_minutes)
@@ -59,35 +53,65 @@ def get_screenings_combinations(screenings: List[CinemaScreening], max_depth = M
     if len(screenings) == 1:
         return [screenings]
     # Separate the screenings into lists by movie
+    screenings_by_movie: dict[str, list[CinemaScreening]] = sort_screenings_into_movies(screenings)
+    combinations = compute_possible_combinations(max_depth, screenings, screenings_by_movie, time_between_screenings)
+    # Order the combinations list by movie name and start time
+    combinations.sort(
+        key=lambda combination: combination[0].movie + str(combination[0].get_start_time_in_human_readable_format()))
+    # Filter combinations with only one screening
+    filtered_combinations = [combination for combination in combinations if len(combination) > 1]
+    return filtered_combinations
+
+
+def compute_possible_combinations(max_depth, screenings, screenings_by_movie, time_between_screenings) -> List[Tuple[CinemaScreening]]:
+    combinations = initialize_combinations_with_single_screenings(screenings)
+    depth = 1
+    while depth < max_depth:
+        new_combinations = set()
+        for combination in combinations:
+            new_combinations = new_combinations.union(extend_combination_with_other_screenings(combination,
+                                                                         screenings_by_movie,
+                                                                         time_between_screenings))
+        combinations = new_combinations.copy()
+        depth += 1
+    return list(combinations)
+
+
+def sort_screenings_into_movies(screenings):
     screenings_by_movie = {}
     for screening in screenings:
         if screening.movie not in screenings_by_movie:
             screenings_by_movie[screening.movie] = []
         screenings_by_movie[screening.movie].append(screening)
-    # As long as we can add a screening to the combinations
-    combinations = []
+    return screenings_by_movie
+
+
+def initialize_combinations_with_single_screenings(screenings):
+    combinations = set()
     for screening in screenings:
-        combinations.append((screening,))
-    depth = 1
-    while depth < max_depth:
-        new_combinations = set()
-        for combination in combinations:
-            anything_added = False
-            for other_movie, other_movie_screenings in screenings_by_movie.items():
-                if other_movie not in [screening.movie for screening in combination]:
-                    first_available_screening = find_first_available_screening(other_movie_screenings, combination[-1], time_between_screenings)
-                    if first_available_screening:
-                        new_combinations.add(combination + (first_available_screening,))
-                        anything_added = True
-            if not anything_added:
-                new_combinations.add(combination)
-        combinations = list(new_combinations)
-        depth+= 1
-    # Order the combinations list by movie name and start time
-    combinations.sort(key=lambda combination: combination[0].movie + str(combination[0].get_start_time_in_human_readable_format()))
-    # Filter combinations with only one screening
-    filtered_combinations = [combination for combination in combinations if len(combination) > 1]
-    return filtered_combinations
+        combinations.add((screening,))
+    return combinations
+
+
+def extend_combination_with_other_screenings(combination: Tuple[CinemaScreening], screenings_by_movie: dict[str, List[CinemaScreening]],
+                                             time_between_screenings: int) -> Set[Tuple[CinemaScreening]]:
+    """
+    Extends the combination in parameter with other screenings of other movies. 
+    Returns either the unchanged combination if no other screenings are available or the new extends possible combinations.
+    """
+    new_combinations = set()
+    anything_added = False
+    for other_movie, other_movie_screenings in screenings_by_movie.items():
+        if other_movie not in [screening.movie for screening in combination]:
+            first_available_screening = find_first_available_screening(other_movie_screenings, combination[-1], time_between_screenings)
+            if first_available_screening:
+                new_combinations.add(combination + (first_available_screening,))
+                anything_added = True
+    if not anything_added:
+        return set((combination,))
+    else:
+        return new_combinations
+
 
 def find_first_available_screening(other_movie_screenings, screening, time_between_screenings):
     for other_screening in other_movie_screenings:
@@ -106,6 +130,7 @@ def read_cinema_screenings_from_csv(file_path):
             screenings.append(CinemaScreening(movie, start_time, int(duration)))
     return screenings
 
+
 def get_screenings_combinations_from_csv(file_path):
     return get_screenings_combinations(read_cinema_screenings_from_csv(file_path))
 
@@ -119,6 +144,7 @@ def does_screening_intersect(screening1: CinemaScreening, screening2: CinemaScre
     :return:
     """
     return screening2.start_time_in_minutes < screening1.get_ending_time_in_minutes() + time_between_screenings
+
 
 def convert_start_time_in_minutes(start_time):
     if isinstance(start_time, str):
